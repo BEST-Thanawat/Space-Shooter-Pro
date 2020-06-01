cc73a1c57dce295fe7ea7b0f32c76684bd0ec8ea
BEST-Thanawat <thanawat.sukwibul@gmail.com>
2020-05-31 21:55:36 -0700

    Add destroy asteriod animation.

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -6,7 +6,8 @@ public class Asteroid : MonoBehaviour
 {
     [SerializeField]
     private float _rotateSpeed = 3.0f;
-
+    [SerializeField]
+    private GameObject _explosionAnim;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +18,15 @@ public class Asteroid : MonoBehaviour
     void Update()
     {
         transform.Rotate(Vector3.forward * _rotateSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Laser")
+        {
+            Instantiate(_explosionAnim, transform.position, Quaternion.identity);
+            Destroy(other);
+            Destroy(gameObject, 0.3f);
+        }
     }
 }
